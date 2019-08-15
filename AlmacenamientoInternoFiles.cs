a69f17047e5f088de09e19440aa82c7f87026fdd
byRamon <46496290+byRamon@users.noreply.github.com>
2019-08-15 12:18:55 -0500

    Agregar archivos de proyecto.

---
+++ AlmacenamientoInternoFiles.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ManejoDeArchivos
+{
+    class AlmacenamientoInternoFiles
+    {
+        string pathAlmacenamientoInterno;
+        string pathAlmacenamientoInternoTemp;
+        public AlmacenamientoInternoFiles(string filesDir, string cacheDir)
+        {
+            pathAlmacenamientoInterno = filesDir;
+            pathAlmacenamientoInternoTemp = cacheDir;
+        }
+
+        public async Task<string> leerArchivo(string fileName)
+        {
+            string pathFile = Path.Combine(pathAlmacenamientoInterno, fileName);
+            string texto = "";
+            if (File.Exists(pathFile))
+            {
+                var reader = new StreamReader(pathFile);
+
+                string linea = "";
+                while ((linea = await reader.ReadLineAsync()) != null)
+                    texto += linea;
+            }
+            return texto;
+        }
+
+        public void escribirArchivo(string fileName, string contenido)
+        {
+            string pathFile = Path.Combine(pathAlmacenamientoInterno, fileName);
+            File.WriteAllText(pathFile, contenido);
+        }
+
+        public string leerArchivoTemporal(string fileNameTmp)
+        {
+            fileNameTmp = fileNameTmp.Insert(fileNameTmp.IndexOf('.'), "_tmp");
+            string pathFileTmp = Path.Combine(pathAlmacenamientoInternoTemp, fileNameTmp);
+            if (File.Exists(pathFileTmp))
+                return File.ReadAllText(pathFileTmp);
+            return "";
+        }
+
+        public void escribirArchivoTemporal(string fileNameTmp, string contenido)
+        {
+            fileNameTmp = fileNameTmp.Insert(fileNameTmp.IndexOf('.'), "_tmp");
+            string pathFileTmp = Path.Combine(pathAlmacenamientoInternoTemp, fileNameTmp);
+            File.WriteAllText(pathFileTmp, contenido);
+        }
+        public void eliminarArchivoTemporal(string fileNameTmp)
+        {
+            fileNameTmp = fileNameTmp.Insert(fileNameTmp.IndexOf('.'), "_tmp");
+            string pathFileTmp = Path.Combine(pathAlmacenamientoInternoTemp, fileNameTmp);
+            File.Delete(fileNameTmp);
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.IO;
+
+namespace ManejoDeArchivos
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        AlmacenamientoInternoFiles aiFiles;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            EditText etContentFile = FindViewById<EditText>(Resource.Id.etContentFile);
+            Button btnAbrirArchivo = FindViewById<Button>(Resource.Id.btnAbrirArchivo);
+
+            //AlmacenamientoInternoFiles aiFiles = new AlmacenamientoInternoFiles(FilesDir.AbsolutePath, CacheDir.AbsolutePath);
+            aiFiles = new AlmacenamientoInternoFiles(FilesDir.AbsolutePath, CacheDir.AbsolutePath);
+            aiFiles.escribirArchivo("archivo1.txt", "Este es el contenido del archivo.\nEsta es la línea 2.\n Y esta es la línea 3.");
+
+            btnAbrirArchivo.Click += async (sender, e) => {
+                etContentFile.Text = "";
+                etContentFile.Text = await aiFiles.leerArchivo("archivo1.txt");
+            };
+
+            Button btnAbrirArchivoTmp = FindViewById<Button>(Resource.Id.btnAbrirArchivoTmp);
+            aiFiles.escribirArchivoTemporal("archivo1.txt", "Este es el contenido del archivo temporal.\nEsta es la línea 2.\n Y esta es la línea 3.");
+            btnAbrirArchivoTmp.Click += delegate {
+                etContentFile.Text = "";
+                etContentFile.Text = "Archivo archivo1_tmp.txt almacenado en: " + System.IO.Path.Combine(CacheDir.AbsolutePath, "archivo1_tmp.txt") + "\n";
+                etContentFile.Text += aiFiles.leerArchivoTemporal("archivo1.txt");
+            };
+
+            Button btnVEAE = FindViewById<Button>(Resource.Id.btnVEAE);
+            btnVEAE.Click += delegate {
+                bool escribible = Android.OS.Environment.ExternalStorageState == Android.OS.Environment.MediaMounted;
+                bool leible = escribible || Android.OS.Environment.ExternalStorageState == Android.OS.Environment.MediaMountedReadOnly;
+                etContentFile.Text = string.Format("El almacenamiento externo {0} esta disponible, {1} es escribible y {2} es leíble", escribible ? "si" : "no", escribible ? "si" : "no", leible ? "si" : "no");
+                etContentFile.Text += string.Format("\nLa ruta del almacenamiento externo es: {0}", GetExternalFilesDir(null).AbsolutePath);
+                etContentFile.Text += string.Format("\nLa ruta del almacenamiento externo temporal es: {0}", ExternalCacheDir.AbsolutePath);
+            };
+            var localFolder = Android.App.Application.Context.GetExternalFilesDir(null).ToString();
+            string[] files = Directory.GetFiles(FilesDir.AbsolutePath, "*.txt", SearchOption.AllDirectories);
+            files = Directory.GetFiles("/storage/emulated/0/", "*.jpeg", SearchOption.AllDirectories);
+            string archivos = "";
+            foreach(string archivo in files)
+            {
+                archivos += (archivos.Length > 0 ? "\n" : "") + archivo;
+            }
+            EditText txtFiles = FindViewById<EditText>(Resource.Id.etFiles);
+            txtFiles.Text = archivos;
+        }
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            aiFiles.eliminarArchivoTemporal("archivo1.txt");
+        }        
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
